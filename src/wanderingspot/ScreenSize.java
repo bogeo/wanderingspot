7361b4356561e6a9b538f9390e76162161329f51
Benno Schmidt <BennoSchmidt@users.noreply.github.com>
2015-09-16 14:15:29 +0200

    Create ScreenSize.java

---
+++ src/wanderingspot/ScreenSize.java
@@ -0,0 +1,14 @@
+package wanderingspot;
+
+/**
+ * Screen-size given in pixels
+ */
+public class ScreenSize 
+{
+	public int width, height;
+
+	public ScreenSize(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+}
